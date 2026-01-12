f497b2db68d020e7e724a2ac5dc754fbe182939a
Admin <prachimshewale26@gmail.com>
2026-01-12 09:59:06 +0530

    response mapping for weather details

---
+++ src/main/java/com/research/weather/SysObj.java
@@ -0,0 +1,11 @@
+package com.research.weather;
+
+import lombok.Data;
+
+@Data
+public class SysObj {
+
+    private String country;
+    private long sunrise;
+    private long sunset;
+}
---
+++ src/main/java/com/research/weather/Weather.java
@@ -0,0 +1,11 @@
+package com.research.weather;
+
+import lombok.Data;
+
+@Data
+public class Weather {
+    private long id;
+    private String main;
+    private String description;
+    private String icon;
+}
